8bfd0584444de41d77426b7017ecf6f823105414
takumi-shibata <241448ta@gmail.com>
2020-03-05 00:48:33 +0000

    Start!

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,18 +4,19 @@ class ApplicationController < ActionController::Base
 	#デバイス機能実行前にconfigure_permitted_parametersの実行をする。
 	protect_from_forgery with: :exception
 
-  protected
-  def after_sign_in_path_for(resource)
-    root_path
-  end
-
+  #def after_sign_in_path_for(resource)
+    #root_path
+  #end
   #sign_out後のredirect先変更する。rootパスへ。rootパスはhome topを設定済み。
-  def after_sign_out_path_for(resource)
-    user_path(resource)
+  
+  def after_sign_in_path_for(resource)
+    user_path(current_user)
   end
 
+  protected
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :email])
     #sign_upの際にnameのデータ操作を許。追加したカラム。
+    devise_parameter_sanitizer.permit(:sign_in, keys: [:name])
   end
 end
